d7cf2c77078dbbb3fb523b9455ab57b916f98a30
Santiago Fuentes <Santigibert6789@gmail.com>
2022-08-30 18:50:25 -0300

    Arreglando menu

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,9 @@
 const NavBar =  () => {
     return (
         <div className="NavBar-contenedor">
-            <div>Inicio</div>
-            <div>Productos</div>
-            <div>Contacto</div>
+            <a className="NavBar-menu" href="#1">Inicio</a>
+            <a className="NavBar-menu" href="#2">Productos</a>
+            <a className="NavBar-menu" href="#3">Contacto</a>
         </div>
     );
 };
