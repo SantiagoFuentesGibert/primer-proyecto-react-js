84efe5ee1945fa323de42fa4c0f95be810ed8541
Santiago Fuentes <Santigibert6789@gmail.com>
2022-08-30 18:43:20 -0300

    Creando NavBar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar =  () => {
+    return (
+        <div className="NavBar-contenedor">
+            <div>Inicio</div>
+            <div>Productos</div>
+            <div>Contacto</div>
+        </div>
+    );
+};
+
+export default NavBar;
