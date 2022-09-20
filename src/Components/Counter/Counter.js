1bbb99fb65e41c060cfbfcba2293249fcb6e863c
Santiago Fuentes <Santigibert6789@gmail.com>
2022-09-19 23:43:11 -0300

    Modificando detalles

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -2,9 +2,10 @@ import React, {useState} from "react";
 import './Counter.styles.css'
 
 const Counter = () => {
+    const stockProductos = 10;
     const stockDisponible = 10;
     const [stock, setStock] = useState(0);
-    const sumar = () => stock <  stockDisponible ? setStock(stock + 1) : alert('Se alcanzo el stock disponible')
+    const sumar = () => stock <  stockProductos ? setStock(stock + 1) : alert('Se alcanzo el stock disponible')
     const restar = () => stock > 0 ? setStock(stock - 1) : alert('No sé pueden introducir valores negativos')
   return (
     <>
